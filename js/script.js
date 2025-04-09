81cfc7fb3165846f241a0429ab2f78ff776f6c85
bguevara-2 <bguevara2@hawk.iit.edu>
2025-04-08 22:48:55 -0500

    adding the window.addEventListener

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 function file(){
   var button = document.getElementById('entrybutton');
 
-
-
   function showText(){
-    var text = document.getElementById('entrybutton');
+    var text = document.getElementById('entryinput');
+    
     alert("Bryan Guevara: " + );
   }
 
   button.addEventListener('click', showText);
 }
+
+window.addEventListener('load', file);
