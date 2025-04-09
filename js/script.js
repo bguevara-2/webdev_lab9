9641ad3293fe875dc7958b4f92dd27fbe57a062e
bguevara-2 <bguevara2@hawk.iit.edu>
2025-04-08 22:46:21 -0500

    adding the getElements to get data from document

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function file(){
+  var button = document.getElementById("entrybutton");
+
+
+
+  function showText(){
+    var text = document.getElementById("entrybutton");
+    alert("Bryan Guevara: " + );
+  }
+
+  button.addEventListener("click", showText);
+}
